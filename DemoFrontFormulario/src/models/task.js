0e54157391215cc43cae3b7596844862f7b06bee
Ubuntu <ubuntu@ip-172-31-21-48.us-east-2.compute.internal>
2020-11-11 21:16:22 +0000

    estructura collecion

--- DemoFrontFormulario/src/models/task.js
+++ DemoFrontFormulario/src/models/task.js
@@ -6,7 +6,20 @@ const { Schema } = mongoose;
  */
 const TaskSchema = new Schema({
     title: { type: String, required: true},
-    description: { type: String, required: true}
+    description: { type: String, required: true}/*,
+    token : {type: String},
+    modelo_primario: { type: String, required: true},
+    modelo_secundario: { type: String, required: true},
+    guardar_metada: {type : Boolean, required: true},
+    camara_id : {type : Number, required: true},
+    video : {
+              tipo: { type: String, required: true},
+              path: {type: String, required: true},
+              url:  {type: String, required: true},
+              ancho:{type: Number, required: true},
+              alto: {type: Number, required: true}
+    }
+    */
 });
 
 /**
