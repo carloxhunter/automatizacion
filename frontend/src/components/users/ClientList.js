6d5fc460ea57d9c49c7904d80bedd533e2768fbc
Carlos Elgueta <carlos.elgueta.13@sansano.usm.cl>
2020-11-27 19:37:21 -0300

    merging

--- frontend/src/components/users/ClientList.js
+++ frontend/src/components/users/ClientList.js
@@ -153,7 +153,7 @@ class ClientList extends Component{
 							{tasks.map((task,index) => (
 								<tr key={task.id}>
 									<th scope="row">{index + 1}</th>
-									<td>{task.estado}</td>
+									<td>{JSON.stringify(task.estado)}</td>
 									<td>{task.id}</td>
 									<td>{task.modelo_primario}</td>
                 					<td>{JSON.stringify(task.modelo_secundario)}</td>
